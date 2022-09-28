032ca292979d5db4d1603412bda68b6e8300cbc5
tizenkethur <you@example.com>
2022-09-28 07:04:57 +0200

    information and picture added about Klára

--- src/db-data.ts
+++ src/db-data.ts
@@ -3,8 +3,19 @@ export const farewellPersons: any = [
     id: 1,
     name: 'Nagy Klára',
     pictureUrl: '',
-    introduction: '',
-    phone: '',
-    email: '',
+    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal. 
+    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam. 
+    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel, 
+    idézetekkel, szép, emberi gondolatokkal – pátosz nélkül – segítsem a gyászolókat fájdalmuk kifejezésében.
+    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától, 
+    kedves ismerősétől.
+    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek, 
+    ha veszteség éri őket, ha ismét búcsúzni kényszerülnek.
+    A visszajelzések alapján a hozzátartozók pozitívan értékelik az empátia, a jó kapcsolatteremtő képesség mellett, – sokéves tapasztalatomat, felkészültségemet; és a nyugalmat, amivel igyekszem átsegíteni a gyászolókat ezen a nehéz órán, amit szerettük utolsó útja jelent számukra.`,
+    quote: '„Az utolsó szavak melege a semmibe is elkísér…”',
+    phone: ' 06 30 252 7559',
+    email: 'nagyklara@gmail.com',
+    web: 'www.bucsuztato.hu',
+    facebook: 'https://www.facebook.com/profile.php?id=100083359909403',
   },
 ];
