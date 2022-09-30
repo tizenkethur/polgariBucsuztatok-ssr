830a6019be67f42f64facfee5eabe7462ca39aad
tizenkethur <you@example.com>
2022-09-30 21:41:54 +0200

    data and pictures added, 3 card are ready

--- src/db-data.ts
+++ src/db-data.ts
@@ -2,58 +2,87 @@ import { farewellPerson } from './app/shared/models/farewellPersons';
 export const farewellPersonData: farewellPerson[] = [
   {
     id: 1,
-    name: 'Nagy Klára',
-    pictureUrl: '../assets/farewellPersons/Nagy_Klára.jpg',
-    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal. 
-    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam. 
-    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel, 
-    idézetekkel, szép, emberi gondolatokkal – pátosz nélkül – segítsem a gyászolókat fájdalmuk kifejezésében.
-    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától, 
-    kedves ismerősétől.
-    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek, 
-    ha veszteség éri őket, ha ismét búcsúzni kényszerülnek.
-    A visszajelzések alapján a hozzátartozók pozitívan értékelik az empátia, a jó kapcsolatteremtő képesség mellett, – sokéves tapasztalatomat, felkészültségemet; és a nyugalmat, amivel igyekszem átsegíteni a gyászolókat ezen a nehéz órán, amit szerettük utolsó útja jelent számukra.`,
-    quote: '„Az utolsó szavak melege a semmibe is elkísér…”',
-    phone: ' 06 30 252 7559',
-    email: 'nagyklara@gmail.com',
-    web: 'www.bucsuztato.hu',
-    facebook: 'https://www.facebook.com/profile.php?id=100083359909403',
+    name: 'Felber Gabriella',
+    pictureUrl: '../assets/farewellPersons/Felber_Gabriella.jpg',
+    introduction: `Felber Gabriella vagyok.
+      2013. óta vezetek polgári búcsúztató szertartásokat.
+      Operaénekes előadóművészként, igen korán ráéreztem az
+      emberekre való ráhangolódás képességére.
+      Már a telefonos egyeztetés során érzékelem, hogy milyen
+      módon és hangvétellel szeretné a család hallani a
+      búcsúztatón az általuk megfogalmazott gondolatokat
+      érzéseket, hiszen ezek a pillanatok számukra is nagyon
+      nehezek és igen fájdalmasak.
+      Mivel minden rezgés, ezért az eltávozó lélek számára
+      nemcsak a beszédhang, hanem az énekhang frekvenciái is
+      fontosak, hiszen segítenek az átlépésben.
+      Ezért, ha csak tehetem, minden egyes búcsúztatáson
+      énekelek is természetesen csakis mindent a családdal
+      egyeztetve.
+      (A dalok repertoáromból való kiválasztásában segítek.)
+      Minden szertartáson úgy tolmácsolom ezeket a gondolatokat,
+      mintha saját szerettemet búcsúztatnám.
+      Mindezt a család is érzi, hiszen a szemükben olykor
+      megcsillanó könnycsepp és a végén elhangzó őszinte, meleg
+      hangú köszönetnyilvánítás is ezt jelzi.
+      Úgy érzem, talán az a legfontosabb küldetésem, hogy a család
+      méltósággal, békében és igaz megnyugvással tudja elengedni
+      és tudjon elköszönni attól a lélektől, aki talán tegnap még itt a
+      földön, a mindent jelentette számukra.
+      Ezt a küldetésemet a legnagyobb alázattal, szeretettel és
+      együttérzéssel teszem.`,
+    quote: '',
+    phone: '06 30 275 2466',
+    email: 'gabigloriella@gmail.com',
+    web: '',
+    facebook: '',
   },
   {
     id: 2,
-    name: 'Nagy Klára',
-    pictureUrl: '../assets/farewellPersons/Nagy_Klára.jpg',
-    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal. 
-    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam. 
-    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel, 
-    idézetekkel, szép, emberi gondolatokkal – pátosz nélkül – segítsem a gyászolókat fájdalmuk kifejezésében.
-    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától, 
-    kedves ismerősétől.
-    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek, 
-    ha veszteség éri őket, ha ismét búcsúzni kényszerülnek.
-    A visszajelzések alapján a hozzátartozók pozitívan értékelik az empátia, a jó kapcsolatteremtő képesség mellett, – sokéves tapasztalatomat, felkészültségemet; és a nyugalmat, amivel igyekszem átsegíteni a gyászolókat ezen a nehéz órán, amit szerettük utolsó útja jelent számukra.`,
-    quote: '„Az utolsó szavak melege a semmibe is elkísér…”',
-    phone: ' 06 30 252 7559',
-    email: 'nagyklara@gmail.com',
-    web: 'www.bucsuztato.hu',
-    facebook: 'https://www.facebook.com/profile.php?id=100083359909403',
+    name: 'Hangay Mária',
+    pictureUrl: '../assets/farewellPersons/Hangay_Marica.jpg',
+    introduction: `Hangay Mária vagyok. Az Eötvös Lóránd Tudományegyetemen diplomáztam
+      esztétika-magyar szakon. 2004-ben az egyetemek között megrendezett országos szépkiejtési
+      versenyen Kazinczy-díjat szereztem, ez megerősített abban, hogy bármerre is sodor majd az
+      élet, az igényes anyanyelvhasználat mindig meghatározó lesz számomra. Az irodalomtanítás
+      mellett immár 15 esztendeje foglalkozom polgári búcsúztatással. Hivatásként tekintek a
+      munkámra. Hiszem, hogy a méltó búcsú egyaránt segíti a távozót és az itt maradottakat.
+  Annak idején szüleim elvesztése irányította figyelmemet a búcsúztatás felé. Akkor
+  értettem meg, hogy az emlékezés nem csupán múltba tekintés, hanem főhajtás is elvesztett
+  szeretteink előtt, s egyúttal az általuk közvetített értékek folytonosságának vállalása is. A
+  tiszta, önzetlen emberi élet emlékei az élők világában tovább gyökereznek. Halottaink nem
+  csak a múltat hagyják itt számunkra, hanem tovább élnek tetteinkben is. Fontosnak tartom,
+  hogy a búcsúztató során méltó módon idézzem fel az elhunyt életét, így kapaszkodót adva az
+  ittmaradottaknak a folytatáshoz.`,
+    quote: `„Mert sehol se vagy, mindenütt kereslek,
+  nap, rét, tó, felhő, száz táj a ruhád,
+  mindig mutat valahol a világ
+  s mindig elkap, bár kereső szememnek
+  tévedései is hozzád vezetnek,
+  úgyhogy fény-árnyak, tündérciterák
+  villantják hangod, a szemed, a szád,
+  csöndes játékait a képzeletnek…” Szabó Lőrinc`,
+    phone: '06 30 951 5816',
+    email: 'hangaymarica@gmail.com',
+    web: '',
+    facebook: '',
   },
   {
     id: 3,
     name: 'Nagy Klára',
     pictureUrl: '../assets/farewellPersons/Nagy_Klára.jpg',
-    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal. 
-    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam. 
-    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel, 
+    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal.
+    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam.
+    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel,
     idézetekkel, szép, emberi gondolatokkal – pátosz nélkül – segítsem a gyászolókat fájdalmuk kifejezésében.
-    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától, 
+    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától,
     kedves ismerősétől.
-    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek, 
+    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek,
     ha veszteség éri őket, ha ismét búcsúzni kényszerülnek.
     A visszajelzések alapján a hozzátartozók pozitívan értékelik az empátia, a jó kapcsolatteremtő képesség mellett, – sokéves tapasztalatomat, felkészültségemet; és a nyugalmat, amivel igyekszem átsegíteni a gyászolókat ezen a nehéz órán, amit szerettük utolsó útja jelent számukra.`,
     quote: '„Az utolsó szavak melege a semmibe is elkísér…”',
-    phone: ' 06 30 252 7559',
-    email: 'nagyklara@gmail.com',
+    phone: '06 30 252 7559',
+    email: 'nagyklarav@gmail.com',
     web: 'www.bucsuztato.hu',
     facebook: 'https://www.facebook.com/profile.php?id=100083359909403',
   },
@@ -61,13 +90,13 @@ export const farewellPersonData: farewellPerson[] = [
     id: 4,
     name: 'Nagy Klára',
     pictureUrl: '../assets/farewellPersons/Nagy_Klára.jpg',
-    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal. 
-    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam. 
-    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel, 
+    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal.
+    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam.
+    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel,
     idézetekkel, szép, emberi gondolatokkal – pátosz nélkül – segítsem a gyászolókat fájdalmuk kifejezésében.
-    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától, 
+    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától,
     kedves ismerősétől.
-    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek, 
+    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek,
     ha veszteség éri őket, ha ismét búcsúzni kényszerülnek.
     A visszajelzések alapján a hozzátartozók pozitívan értékelik az empátia, a jó kapcsolatteremtő képesség mellett, – sokéves tapasztalatomat, felkészültségemet; és a nyugalmat, amivel igyekszem átsegíteni a gyászolókat ezen a nehéz órán, amit szerettük utolsó útja jelent számukra.`,
     quote: '„Az utolsó szavak melege a semmibe is elkísér…”',
@@ -80,13 +109,13 @@ export const farewellPersonData: farewellPerson[] = [
     id: 5,
     name: 'Nagy Klára',
     pictureUrl: '../assets/farewellPersons/Nagy_Klára.jpg',
-    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal. 
-    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam. 
-    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel, 
+    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal.
+    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam.
+    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel,
     idézetekkel, szép, emberi gondolatokkal – pátosz nélkül – segítsem a gyászolókat fájdalmuk kifejezésében.
-    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától, 
+    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától,
     kedves ismerősétől.
-    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek, 
+    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek,
     ha veszteség éri őket, ha ismét búcsúzni kényszerülnek.
     A visszajelzések alapján a hozzátartozók pozitívan értékelik az empátia, a jó kapcsolatteremtő képesség mellett, – sokéves tapasztalatomat, felkészültségemet; és a nyugalmat, amivel igyekszem átsegíteni a gyászolókat ezen a nehéz órán, amit szerettük utolsó útja jelent számukra.`,
     quote: '„Az utolsó szavak melege a semmibe is elkísér…”',
@@ -99,13 +128,13 @@ export const farewellPersonData: farewellPerson[] = [
     id: 6,
     name: 'Nagy Klára',
     pictureUrl: '../assets/farewellPersons/Nagy_Klára.jpg',
-    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal. 
-    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam. 
-    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel, 
+    introduction: `1992 óta foglakozom polgári búcsúztatással, ezzel a nehéz, ám fontos és szép feladattal.
+    Pedagógus végzettséget szereztem, pszichológiát tanultam, és énekművészi tanulmányokat folytattam.
+    A hangképzésnek, beszédművelésnek ma is, – napi szinten – hasznát veszem a munkám során. Kezdettől arra törekszem, hogy – versekkel,
     idézetekkel, szép, emberi gondolatokkal – pátosz nélkül – segítsem a gyászolókat fájdalmuk kifejezésében.
-    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától, 
+    Célom, hogy a búcsúzást követően mindenki megnyugvással térhessen haza, – azzal az érzéssel, hogy méltóképpen, szépen köszönhetett el hozzátartozójától, barátjától,
     kedves ismerősétől.
-    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek, 
+    Az évek során számos család búcsúzott hozzátartozójától a közreműködésemmel – és igen sokan, – akár hosszú évek múltán, több alkalommal is – újra megkeresnek,
     ha veszteség éri őket, ha ismét búcsúzni kényszerülnek.
     A visszajelzések alapján a hozzátartozók pozitívan értékelik az empátia, a jó kapcsolatteremtő képesség mellett, – sokéves tapasztalatomat, felkészültségemet; és a nyugalmat, amivel igyekszem átsegíteni a gyászolókat ezen a nehéz órán, amit szerettük utolsó útja jelent számukra.`,
     quote: '„Az utolsó szavak melege a semmibe is elkísér…”',
